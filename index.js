e5ae2ee6d4bc7dddf792ed3c424ebe6499179b2b
manavendrasen <manavendra.199302058@muj.manipal.edu>
2020-07-03 14:00:46 +0530

    Added sky-bound collision + mobile responsive collison

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
+// import { reset } from "./resetGame";
+
 const player = document.querySelector(".player");
 const playableArea = document.querySelector(".playable-area");
 let pixels = 20;
 
 console.log(screen.height);
+
 //gravity
 var falling;
 falling = setInterval(() => {
-  if (checkGroundCollision(showCoordinate())) {
+  if (checkBorderCollision(showCoordinate())) {
     //if true = the body has collided and we need to shop falling
     stopFalling();
   }
   player.style.transform = `translateY(${pixels}px)`;
   pixels += 20;
-}, 100); // The number indicates the no. of times the player will be pushed below
+}, 50); // The number indicates the no. of times the player will be pushed below
 
 function stopFalling() {
   clearInterval(falling);
-  alert("game over");
+  document.querySelector(".gameover").classList.add("make-visible"); //shows the game over screen
 }
 
 function push() {
-  player.style.transform = `translateY(${pixels - 750}px)`;
-  pixels -= 750;
+  if (screen.height > 768) {
+    player.style.transform = `translateY(${pixels - 750}px)`;
+    pixels -= 750;
+  } else {
+    player.style.transform = `translateY(${pixels - 600}px)`;
+    pixels -= 600;
+  }
 }
-
 document.querySelector(".playable-area").addEventListener("click", push); // pushes the player up when clicked in the screen
 
-//Collision detection with ground
+//Collision detection with Border
 function showCoordinate() {
   var position = player.getBoundingClientRect();
   let y = position.top;
@@ -34,16 +41,29 @@ function showCoordinate() {
   return [x, y];
 }
 
-function checkGroundCollision([x, y]) {
-  let groundCoordinate = screen.height - 300;
+function checkBorderCollision([x, y]) {
+  let groundCoordinate = screen.height;
+  let skyCorrdinate = 0;
+  if (screen.height > 768) {
+    groundCoordinate = screen.height - 300;
+    skyCorrdinate -= 300;
+  }
 
-  if (y >= groundCoordinate) {
-    //the body is below ground level
+  if (y >= groundCoordinate || y <= skyCorrdinate) {
+    //the body is below ground level or above screen
     console.log(screen.height + " " + y);
     playableArea.removeChild(player);
     return true; // Yes the body is below ground level We need to stop
   } else {
-    console.log("else wlal");
+    console.log(screen.height + " " + y);
     return false; //The body is on screen we are fine
   }
+}
+//reset game
+document.querySelector(".reset-button").addEventListener("click", reset);
+function reset() {
+  // player.style.bottom = "500px";
+  // playableArea.appendChild(player);
+  // document.querySelector(".gameover").classList.remove("make-visible");
+  location.reload();
 }
